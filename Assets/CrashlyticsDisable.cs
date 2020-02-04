76fd53df340e103a25cef88897727fd6c04a47e8
Artem <incontumaciam@gmail.com>
2020-02-04 16:22:50 +0300

    disable Crashlytics on Awake

---
+++ Assets/CrashlyticsDisable.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashlyticsDisable : MonoBehaviour
+{
+    private void Awake()
+    {
+        Firebase.Crashlytics.Crashlytics.IsCrashlyticsCollectionEnabled = false;
+    }
+}
